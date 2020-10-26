798f6bf09a6c92b48fa6766e937132b210bfdda7
algok2 <44625038+algok2@users.noreply.github.com>
2020-10-26 22:38:27 +0900

    10:37

---
+++ Samsung01/src/Solution.java
@@ -0,0 +1,36 @@
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "¾È";
+		String str2 = "³ç";
+		String str3 = "ÇÏ";
+		String str4 = "¼¼";
+		String str5 = "¿ä";
+		String str7 = str1 + str2;
+		
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(i);
+			i +=3;
+		}
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		for (int j = 0; j < args.length; j++) {
+			
+		}
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+		System.out.println("Hellow ;;;;dfkjkljdfkl;Java");
+	}
+
+}
